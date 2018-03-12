1d063cc50cf0ca9a7118bd2158f44cbe6a74f0e3
Louis Evans <louisthecool@yahoo.co.uk>
2018-03-12 13:42:57 +0000

    12th March Lunchtime Commit 1

--- app/src/main/java/com/peakevans/localadmin/notificationfaker/MainActivity.java
+++ app/src/main/java/com/peakevans/localadmin/notificationfaker/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,14 +39,16 @@ public class MainActivity extends AppCompatActivity {
 
         Intent notificationIntent = new Intent(this, MainActivity.class);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setSmallIcon(R.drawable.snapicon)
                 .setAutoCancel(true)
                 .setTicker("tickerText")
-                .setContentText("contentText")
+                .setContentText("from Louis E")
                 .setContentIntent(PendingIntent.getActivity(this, 0, notificationIntent, 0))
                 .setWhen(System.currentTimeMillis())
-                .setContentTitle("contentTitle")
+                .setContentTitle("Snapchat")
+                .setColor(Color.RED)
                 .setDefaults(Notification.DEFAULT_ALL);
+
         notification = builder.build();
 
         Handler handler = new Handler();
@@ -61,11 +65,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void pushNotification(){
 
+        int randomInt = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
+
         ((NotificationManager)this.
 
                 getSystemService(NOTIFICATION_SERVICE)).
 
-                notify(0,notification);
+                notify(randomInt, notification);
 
     }
 }
