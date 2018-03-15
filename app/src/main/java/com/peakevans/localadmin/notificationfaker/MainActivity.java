ed5385b3a39eb44de923c580f99f0ccab2f5b22b
Louis Evans <louisthecool@yahoo.co.uk>
2018-03-15 12:43:41 +0000

    15th march p4

--- app/src/main/java/com/peakevans/localadmin/notificationfaker/MainActivity.java
+++ app/src/main/java/com/peakevans/localadmin/notificationfaker/MainActivity.java
@@ -3,22 +3,21 @@ package com.peakevans.localadmin.notificationfaker;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Handler;
+import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
-    //private Button button1;
-    //button1 = (Button) findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
     public Notification notification;
 
+    public String snapper;
+    public int waitTime;
+
+
     public void onClick_new(View view){
 
-        Intent intent = new Intent(this, NewNotification.class);
-        startActivity(intent);
+        EditText snapperInput = (EditText) findViewById(R.id.snapperInput);
+        snapper = snapperInput.getText().toString();
 
+        EditText timeInput = (EditText)findViewById(R.id.timeInput);
+        waitTime = Integer.parseInt(timeInput.getText().toString());
+
+        String message = "Snap created- name: "+snapper+", time: "+waitTime+" seconds";
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
     public void onClick_notify(View view) {
 
         Intent notificationIntent = new Intent(this, MainActivity.class);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.snapicon)
+                .setSmallIcon(R.drawable.snapicon2)
                 .setAutoCancel(true)
                 .setTicker("tickerText")
-                .setContentText("from Louis E")
+                .setContentText("from "+snapper)
                 .setContentIntent(PendingIntent.getActivity(this, 0, notificationIntent, 0))
                 .setWhen(System.currentTimeMillis())
                 .setContentTitle("Snapchat")
@@ -60,8 +68,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-        }, 2000);
+        }, waitTime*1000);
+
     }
+
 
     public void pushNotification(){
 
@@ -73,7 +83,14 @@ public class MainActivity extends AppCompatActivity {
 
                 notify(randomInt, notification);
 
+                PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+                PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "Tag");
+                wakeLock.acquire(5000);
+                wakeLock.release();
+
     }
+
 }
+
 
 
